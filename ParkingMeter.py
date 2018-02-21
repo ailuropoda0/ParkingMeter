95b05561f2900b563249f17d8a5a27b03f78c26a
ailuropoda <ailuropoda0@gmail.com>
2018-02-22 08:36:42 +0900

    Construct 'plot' function

--- ParkingMeter.py
+++ ParkingMeter.py
@@ -3,12 +3,24 @@ import json
 import matplotlib.pyplot as plt
 
 
-def get_api_data(method = ""):
+def get_api_data(method=""):
     api_url = "https://data.sfgov.org/resource/2iym-9kfb.json"
     url = api_url + '?' + method.replace(" ", "%20")
     print(url)
     data = urllib.request.urlopen(url).read().decode()
     return json.loads(data)
+
+
+def plot_bar_graph(x, y, title="", xlabel="", ylabel=""):
+    plt.bar(range(len(y)), y, align='center')
+    plt.xticks(range(len(x)), x, rotation=30)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.subplots_adjust(bottom=0.15)
+    plt.title(title)
+    plt.savefig('figure')
+    plt.show()
+
 
 # Get data via API
 query = '$query=SELECT ratearea as area, count(*) GROUP BY ratearea ORDER BY count DESC'
@@ -18,11 +30,4 @@ area_list = [i.get('area') for i in parking_meter_data]
 num_list = [int(i.get('count', 0)) for i in parking_meter_data]
 
 # Visualization
-plt.bar(range(len(num_list)), num_list, align='center')
-plt.xlabel('AREA')
-plt.ylabel('Nnumber of Parking Meters')
-plt.subplots_adjust(bottom=0.15)
-plt.xticks(range(len(area_list)), area_list, rotation=30)
-plt.title('Total parking meters per AREA')
-plt.savefig('figure')
-plt.show()
+plot_bar_graph(area_list, num_list, 'Total parking meters per AREA', 'AREA', 'Number of parking Meters')
